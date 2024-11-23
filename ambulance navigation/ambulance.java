4d276a49e196f7eddb094db7c88177731781d475
Darshana Mahajan <mahajandarshana299@gmail.com>
2024-11-23 22:08:21 +0530

    Add files via upload

---
+++ ambulance navigation/ambulance.java
@@ -0,0 +1,11 @@
+package com.ambulancenav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulanceNavigationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmbulanceNavigationApplication.class, args);
+    }
+}
